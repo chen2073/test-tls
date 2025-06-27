eb4263b4e09beed62cbec639e51e42667ca47543
James Chen <jchen@cpacketnetworks.com>
2025-06-27 15:54:44 -0700

    merge dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9
+FROM python:3.9 as python
 
 # Install required packages
 RUN apt-get update && apt-get install -y \
@@ -13,3 +13,20 @@ RUN openssl req -x509 -newkey rsa:4096 -keyout /certs/key.pem -out /certs/cert.p
     -days 365 -nodes -subj "/C=US/ST=State/L=City/O=Organization/CN=server"
 
 WORKDIR /app
+
+FROM rust:1.85 as rust
+
+RUN apt-get update && apt-get install build-essential libssl-dev pkg-config -y && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN apt-get update && apt install -y openssl
+
+WORKDIR /app
+
+COPY Cargo.toml Cargo.lock ./
+
+COPY src ./src
+
+RUN cargo build --release
+
+CMD ["/app/target/release/rust_client"]
--- server/server.py
+++ server/server.py
@@ -5,26 +5,42 @@ import ssl
 HOST = os.environ.get("HOST")
 PORT = int(os.environ.get("PORT"))
 
-context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.load_cert_chain(certfile="/certs/cert.pem", keyfile="/certs/key.pem")
+def tls_server():
+    context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    context.load_cert_chain(certfile="/certs/cert.pem", keyfile="/certs/key.pem")
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# server_socket.setblocking(False)
-server_socket.bind((HOST, PORT))
-server_socket.listen(1)
+    # server_socket.setblocking(False)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(1)
 
-conn, addr = server_socket.accept()
-conn_stream = context.wrap_socket(conn, server_side=True)
-try:
-    print(f"Connected by {addr}")
-    while True:
-        data = conn_stream.recv(1024)
-        if not data:
-            break
-        conn_stream.sendall(data)
-except Exception as e:
-    print(f"err with tcp ssl: {e}")
-finally:
-    conn_stream.shutdown(socket.SHUT_RDWR)
-    conn_stream.close()
+    conn, addr = server_socket.accept()
+    conn_stream = context.wrap_socket(conn, server_side=True)
+    try:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn_stream.recv(1024)
+            if not data:
+                break
+            conn_stream.sendall(data)
+    except Exception as e:
+        print(f"err with tcp ssl: {e}")
+    finally:
+        conn_stream.shutdown(socket.SHUT_RDWR)
+        conn_stream.close()
+
+def tcp_server():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen(1)
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+
+tcp_server()
